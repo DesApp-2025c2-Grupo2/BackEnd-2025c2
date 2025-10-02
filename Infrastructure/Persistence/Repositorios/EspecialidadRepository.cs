3324c5e1373d83f115275a5473cd0f91cad2fbe7
DaamiAle <daamiale.recoil@gmail.com>
2025-10-02 17:56:02 -0300

    wip(Especialidad): Repository (persistencia)

---
+++ Infrastructure/Persistence/Repositorios/EspecialidadRepository.cs
@@ -0,0 +1,91 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositorios;
+
+public class EspecialidadRepository : IEspecialidadRepository
+{
+    private readonly ProjectContext context;
+    public EspecialidadRepository(ProjectContext projectContext)
+    {
+        context = projectContext;
+    }
+
+    public async Task<Especialidad> AddAsync(Especialidad especialidad)
+    {
+        try
+        {
+            await context.Especialidades.AddAsync(especialidad);
+            await context.SaveChangesAsync();
+            return especialidad;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error al agregar la Especialidad", ex);
+        }
+    }
+
+    public async Task<List<Especialidad>> GetAllAsync()
+    {
+        try
+        {
+            List<Especialidad> list = await context.Especialidades.ToListAsync();
+            return list;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error al obtener las Especialidades", ex);
+        }
+
+    }
+
+    public async Task<bool> ToggleStatusAsync(int id)
+    {
+        Especialidad? especialidad = await context.Especialidades.FindAsync(id);
+        if (especialidad == null)
+        {
+            throw new Exception("Especialidad no encontrada");
+        }
+        try
+        {
+            if (especialidad.Baja == null)
+            {
+                especialidad.Baja = DateTime.Now.Date;
+            }
+            else
+            {
+                especialidad.Baja = null;
+            }
+            context.Especialidades.Update(especialidad);
+            await context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error al actualizar el estado de la Especialidad", ex);
+        }
+    }
+
+    public async Task<Especialidad> UpdateAsync(Especialidad especialidad)
+    {
+        Especialidad? existingEspecialidad = await context.Especialidades.FindAsync(especialidad.Id);
+        if (existingEspecialidad == null)
+        {
+            throw new Exception("Especialidad no encontrada");
+        }
+        try
+        {
+            existingEspecialidad.Nombre = especialidad.Nombre;
+            existingEspecialidad.Descripcion = especialidad.Descripcion;
+            context.Especialidades.Update(existingEspecialidad);
+            await context.SaveChangesAsync();
+            return existingEspecialidad;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error al actualizar la Especialidad", ex);
+        }
+    }
+}
