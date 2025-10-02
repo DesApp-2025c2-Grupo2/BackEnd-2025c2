c1c90ccfd4792bf64bb6aac30a86b91e7c9da5b8
DaamiAle <daamiale.recoil@gmail.com>
2025-10-02 11:11:53 -0300

    wip(SituacionesTerapeuticas): Inyeccion de dependencias

--- WebAPI/DependencyContainer.cs
+++ WebAPI/DependencyContainer.cs
@@ -1,4 +1,9 @@
-﻿namespace WebAPI;
+﻿using Application.Contracts.Interfaces;
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Persistence.Repositorios;
+
+namespace WebAPI;
 
 public static class DependencyContainer
 {
@@ -6,12 +11,14 @@ public static class DependencyContainer
     {
         // Ejemplo:
         //services.AddScoped(typeof(IUserService), typeof(UserService));
+        services.AddScoped(typeof(ISituacionTerapeuticaService), typeof(SituacionTerapeuticaService));
         return services;
     }
     public static IServiceCollection AddRepositories(this IServiceCollection services)
     {
         // Ejemplo:
         //services.AddScoped(typeof(IUserRepository), typeof(UserRepository));
+        services.AddScoped(typeof(ISituacionTerapeuticaRepository), typeof(SituacionTerapeuticaRepository));
         return services;
     }
 
