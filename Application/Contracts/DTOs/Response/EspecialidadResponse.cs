df6f7b05b261b26d746c426f313d5705b9cecdd2
DaamiAle <daamiale.recoil@gmail.com>
2025-10-02 00:20:57 -0300

    wip(Especialidades): Request y Response DTOs

---
+++ Application/Contracts/DTOs/Response/EspecialidadResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Contracts.DTOs.Response;
+
+public class EspecialidadesResponse : List<EspecialidadResponse> { }
+public class EspecialidadResponse
+{
+    public int id { get; set; }
+    public string nombre { get; set; }
+    public string descripcion { get; set; }
+    public bool activa { get; set; }
+}
