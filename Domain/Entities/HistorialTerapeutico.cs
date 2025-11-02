26cd0be307de9534c8d1c914d4b6d57d114a9de6
Damian Bruque <dbruque@galiciaseguros.com.ar>
2025-11-02 15:19:49 -0300

    wip(Personas): Fecha por situacion terapeutica

---
+++ Domain/Entities/HistorialTerapeutico.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities;
+
+public class HistorialTerapeutico
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    [ForeignKey(nameof(Persona))]
+    public int PersonaId { get; set; }
+    public Persona Persona { get; set; }
+    [Required]
+    [ForeignKey(nameof(SituacionTerapeutica))]
+    public int SituacionTerapeuticaId { get; set; }
+    public SituacionTerapeutica SituacionTerapeutica { get; set; }
+    public DateTime? FechaFin { get; set; }
+}
--- Infrastructure/Persistence/Repositorios/PersonaRepository.cs
+++ Infrastructure/Persistence/Repositorios/PersonaRepository.cs
@@ -22,7 +22,7 @@ namespace Infrastructure.Persistence.Repositorios
             await _context.Personas.AddAsync(persona);
         }
 
-        public async Task<IEnumerable<Persona>> GetByAfiliadoIdAsync(int afiliadoId)
+        public async Task<List<Persona>> GetByAfiliadoIdAsync(int afiliadoId)
         {
             return await _context.Personas
             .Where(p => p.AfiliadoId == afiliadoId)
@@ -37,11 +37,12 @@ namespace Infrastructure.Persistence.Repositorios
                 .Include(p => p.Emails)
                 .Include(p => p.Documentacion)
                 .Include(p => p.Direcciones)
-                .Include(p => p.SituacionesTerapeuticas)
+                // incluimos las situaciones terapeuticas asociadas siempre y cuando la FechaFin sea null o mayor a la sysdate, obviamente truncamos las fechas para que ignoren el horario
+                .Include(p => p.SituacionesTerapeuticas.Where(st => st.FechaFin == null || st.FechaFin > DateTime.Now.Date))
                 .FirstOrDefaultAsync(p => p.Id == id);
         }
 
-        public async Task<IEnumerable<Persona>> GetAllAsync()
+        public async Task<List<Persona>> GetAllAsync()
         {
             return await _context.Personas
                 .Include(p => p.Telefonos)
