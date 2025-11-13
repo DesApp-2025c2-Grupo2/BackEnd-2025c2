91b1bc4076ecac96bba7b58e1d3d0ea9a41e75df
DaamiAle <daamiale.recoil@gmail.com>
2025-11-13 11:09:01 -0300

    wip(reportes): update estructura

---
+++ Infrastructure/Persistence/DBObjects/DBObjectsManager.cs
@@ -0,0 +1,14 @@
+﻿using Application.Utilities;
+using Infrastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.StoredProcedures;
+
+public static class DBObjectsManager
+{
+    public static async Task StarterAsync(ProjectContext context, IProjectLogger logger)
+    {
+       
+    }
+
+}
