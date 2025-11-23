8eb9a980dbfc16c04c900097edddb93f16364c3e
DaamiAle <daamiale.recoil@gmail.com>
2025-11-23 02:15:06 -0300

    wip: service de reportes

---
+++ Application/Contracts/Interfaces/IReporteService.cs
@@ -0,0 +1,25 @@
+﻿using Application.Contracts.DTOs.Request;
+using Application.Contracts.DTOs.Response;
+
+namespace Application.Contracts.Interfaces;
+
+public interface IReporteService
+{
+    /// <summary>
+    /// Devuelve todos los reportes generados alguna vez.
+    /// </summary>
+    /// <returns></returns>
+    Task<ReportesResponse> GetAllAsync();
+    /// <summary>
+    /// Devuelve un reporte del historial.
+    /// </summary>
+    /// <param name="hexaId"></param>
+    /// <param name="tipo"></param>
+    Task<byte[]> RetrieveAsync(string hexaId, int tipo);
+    /// <summary>
+    /// Genera un reporte.
+    /// </summary>
+    /// <param name="reporteRequest"></param>
+    /// <returns></returns>
+    Task<(string, byte[])> GenerateAsync(ReporteRequest reporteRequest);
+}
