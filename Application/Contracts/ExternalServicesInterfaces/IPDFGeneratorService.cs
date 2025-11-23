0b72adf7bdea197eed95e7548c139567cb2194f0
DaamiAle <daamiale.recoil@gmail.com>
2025-11-23 02:18:08 -0300

    wip: adapter generador de pdf

---
+++ Application/Contracts/ExternalServicesInterfaces/IPDFGeneratorService.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace Application.Contracts.ExternalServicesInterfaces;
+
+public interface IPDFGeneratorService
+{
+    byte[] GenerateAltaAfiliadosAsync((string, DataTable) dataTable);
+    byte[] GenerateAltaPrestadoresAsync((string, DataTable) dataTable);
+    byte[] GeneratePrestadoresPorEspecialidadYCodigoPostalAsync((string, DataTable) dataTable);
+    byte[] GenerateSituacionesTerapeuticasPorAfiliadoAsync((string, DataTable) dataTable);
+    byte[] GeneratePrestadoresSinAgendasAsync((string, DataTable) dataTable);
+
+}
